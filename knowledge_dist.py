380c87b34f3ecc8e325fbc3417c0a74a02adaad4
Pavel <p.shahmin@gmail.com>
2024-01-30 16:09:07 +0300

    feat: updated architecture of ConvNet fix: knowledge distillation loss fn and training cycle

--- knowledge_dist.py
+++ knowledge_dist.py
@@ -4,7 +4,6 @@ from torch import nn, optim
 from torchvision import transforms, datasets, models
 from tqdm import tqdm
 from models.ConvNet import ConvNet
-from models.ImageDatasetFullyRAM import ImageDatasetFullyRAM
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
@@ -20,17 +19,17 @@ def train_test_dataloader(fp='data'):
         ]
     )
 
-    # trainset = datasets.ImageFolder(root=fp + "/train", transform=transform2)
-    trainset = ImageDatasetFullyRAM(fp + "/train", transform=transform2)
-    _trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+    trainset = datasets.ImageFolder(root=fp + "/train", transform=transform2)
+    # trainset = ImageDatasetFullyRAM(fp + "/train", transform=transform2)
+    _trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
     testset = datasets.ImageFolder(root=fp + "/test", transform=transform2)
-    _testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
+    _testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
 
     return _trainloader, _testloader
 
 
-def load_trained_resnet(fp='artifacts/resnet34_0.81_on20epoch_adam(0.0001), batch_size 16 Imagenet.pth'):
+def load_trained_resnet(fp='artifacts/resnet_09964_sd.pt'):
     """Loads a pretrained resnet34 for knowledge distillation"""
     resnet = models.resnet34(pretrained=True)
     resnet.fc = nn.Sequential(
@@ -48,7 +47,64 @@ def distillation_loss(_student_output, _teacher_output, _t=2.0):
     """Loss function for knowledge distillation"""
     teacher_probs = torch.sigmoid(_teacher_output / _t)
     student_probs = torch.sigmoid(_student_output / _t)
-    return nn.functional.mse_loss(student_probs, teacher_probs)
+    return nn.functional.mse_loss(student_probs, teacher_probs) * _t ** 2
+
+
+def validate_model(_model, _testloader):
+    """Uses a dataloader to validate the model. Outputs model's accuracy on a given dataset"""
+    _model.eval()
+    class_correct = [0, 0]
+    class_total = [0, 0]
+    # classes = ['Real', 'Fake']
+    _model.eval()
+    with torch.no_grad():
+        for data in tqdm(_testloader, leave=False, position=0):
+            images, labels = data
+            images = images.to(device)
+
+            y_pred = _model(images)
+            predicted = torch.squeeze(torch.round(y_pred))
+            c = predicted.cpu().detach() == labels
+
+            for i, label in enumerate(labels):
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    print(class_correct, class_total)
+    _acc = sum(class_correct) / sum(class_total)
+    return _acc
+
+
+def know_dist_epoch(_model, _epoch_n, alpha=0.1):
+    """Run one epoch of training on a given model inplace
+     _epoch_n is used to print the epoch number
+     alpha is a coefficient for knowledge distillation"""
+    _model.train()
+    running_loss = 0
+    with tqdm(trainloader, leave=False, position=0) as t:
+        for i, (images, labels) in enumerate(t):
+            images, labels = images.to(device), labels.to(device)
+            labels = labels.float()
+
+            with torch.no_grad():
+                teacher_output = teacher_model(images)
+
+            student_output = _model(images)
+
+            true_loss = loss_fn(student_output, labels.unsqueeze(1))
+
+            distill_loss = distillation_loss(student_output, teacher_output)
+
+            loss = alpha * true_loss + (1 - alpha) * distill_loss
+            running_loss += loss
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            batch_to_print = 100
+            if i % batch_to_print == batch_to_print - 1:
+                t.set_description(f'Epoch: {_epoch_n}, loss: {running_loss / batch_to_print}')
+                running_loss = 0
 
 
 trainloader, testloader = train_test_dataloader()
@@ -60,64 +116,15 @@ student_model = ConvNet()
 teacher_model = teacher_model.to(device)
 student_model = student_model.to(device)
 
-optimizer = optim.Adam(student_model.parameters(), lr=0.00003)
+optimizer = optim.Adam(student_model.parameters(), lr=0.001)
 
 accs = []
 
-for epoch in tqdm(range(1), leave=False):
-    student_model.train()
-    running_loss = 0
-    for i, (images, labels) in enumerate(tqdm(trainloader)):
-        images, labels = images.to(device), labels.to(device)
-        labels = labels.float()
-
-        with torch.no_grad():
-            teacher_output = teacher_model(images)
-
-        student_output = student_model(images)
-
-        true_loss = loss_fn(student_output, labels.unsqueeze(1))
-
-        distill_loss = distillation_loss(student_output, teacher_output)
-
-        loss = 0.5 * true_loss + 5 * distill_loss
-        running_loss += loss
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        if i % 25 == 24:
-            # print('On step: ', i, 'current loss: ', round(running_loss / 25, 2))
-            # print(f'On step {i} current loss: {(running_loss / 25):.2f}')
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 25))
-            running_loss = 0
-
-    class_correct = [0, 0]
-    class_total = [0, 0]
-    classes = ['Real', 'Fake']
+for epoch in tqdm(range(3), leave=False):
+    know_dist_epoch(student_model, epoch)
     student_model.eval()
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data
-            images = images.to(device)
-
-            y_pred = student_model(images)
-
-            predicted = torch.squeeze(torch.round(y_pred))
-            # print(predicted)
-            # print(labels)
-            c = predicted.cpu().detach() == labels
-
-            # print(labels)
-            for i, label in enumerate(labels):
-                class_correct[label] += c[i].item()
-                class_total[label] += 1
-
-    print(class_correct, class_total)
-    acc = sum(class_correct) / sum(class_total)
-    print(acc)
+    acc = validate_model(student_model, testloader)
     accs.append(acc)
 
 print(accs)
-torch.save(student_model.state_dict(), 'artifacts/student_pro.pt')
+torch.save(student_model.state_dict(), f'artifacts/student_pro_{accs[-1]}.pt')
--- models/ConvNet.py
+++ models/ConvNet.py
@@ -1,43 +1,28 @@
 from torch import nn
+from torch import flatten
 import torch.nn.functional as F
 
 
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.avpool = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
-
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=2, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        # self.dropout = nn.Dropout(p=0.2)
-        self.fc1 = nn.Linear(128 * 4 * 4, 512)
-        self.fc2 = nn.Linear(512, 1)
-        # self.fc3 = nn.Linear(30, 1)
-        # self.fc2_2 = nn.Linear(1000, 1)
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=5, padding=1, stride=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=5, stride=3),
+            nn.Conv2d(32, 64, kernel_size=5, padding=1, stride=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=5, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(1600, 256),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(256, 1)
+        )
 
     def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-
-        skip = x
-        x = F.relu(self.bn1(self.conv2(x)))
-        x = self.bn1(self.conv2(x))
-        x += skip
-        x = F.relu(x)
-        x = self.avpool(x)
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.pool(F.relu(self.pool(self.conv4(x))))
-
-        # print(x.shape)
-
-        x = x.view(-1, 4 * 4 * 128)
-        # x = self.dropout(x)
-        x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        x = F.sigmoid(self.fc2(x))
+        x = self.features(x)
+        x = flatten(x, 1)
+        x = self.classifier(x)
         return x
